7b594534250add7da08d37e60ac4f35f16925f43
Erika Espejo <erikaespejo95@gmail.com>
2021-05-16 18:23:19 -0500

    middleware: logger,  authenticator & localization

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const fs = require("fs");
 const path = require("path");
-//const { localization } = require("./middleware/localization");
+const { localization } = require("./middleware/localization");
 
 //const users = require("./users/router");
 //const tweets = require("./tweets/router");
@@ -18,9 +18,8 @@ const logStream = fs.createWriteStream(path.join(__dirname, accessLogDir), {
 
 router.use(helmet());
 router.use(morgan("combined", { stream: logStream }));
-//router.use(localization);
+router.use(localization);
 //router.use("/users", users);
 //router.use("/tweets", tweets);
-//router.use("/weather", weather);
 
 module.exports = router;
